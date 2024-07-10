47ddc9141d3bd64d44f2f0ea7d2dde88f834e1c1
Andres <andreees.ys@gmail.com>
2024-07-10 20:49:47 +0200

    Temporal series

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -16,7 +16,7 @@ with rasterio.open(file_path) as dataset:
     matrix = matrix.reshape((rows*cols, nbands))
     '''
     # Read band
-    band = dataset.read(4) # Change the number to read a different band if needed
+    band = dataset.read(8) # Change the number to read a different band if needed
     no_data_value = dataset.nodata  # Get the no-data value for the band
 
     # Mask the no-data values
