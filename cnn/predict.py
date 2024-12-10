0a8491d1a191957d7f986c7927373a44841da417
Andres <andreees.ys@gmail.com>
2024-12-10 23:38:54 +0200

    UHI predictions with CNN

---
+++ cnn/predict.py
@@ -0,0 +1,52 @@
+from smooth_tiled_predictions import predict_img_with_smooth_windowing
+import rasterio
+import torch
+import numpy as np
+from model import CNN
+
+input_path = './data/raw/predictors_skf_imd_ndvi_small.tif'
+output_path = './data/result.tif'
+model_path = 'checkpoint.pth.tar'
+
+with rasterio.open(input_path) as src:
+    # Read all bands from input and target rasters
+    input_img = src.read() # Shape: (3, H, W)
+
+# Smooth blending parameters
+window_size = 160
+nb_classes = 10
+
+# Load the checkpoint
+model = CNN()
+model.load_state_dict(torch.load(model_path, weights_only=True))
+model.eval()
+
+'''predictions_smooth = predict_img_with_smooth_windowing(
+    input_img,
+    window_size=window_size,
+    subdivisions=2, # Minimal amount of overlap for windowing. Must be an even number.
+    nb_classes=nb_classes,
+    pred_func=(
+        lambda img_batch_subdiv: model.predict(img_batch_subdiv)
+    )
+)'''
+
+patch_size = 64
+stride = patch_size
+_, height, width = input_img.shape
+result = np.zeros((height, width))
+
+for i in range(0, height - patch_size + 1, stride):
+    for j in range(0, width - patch_size + 1, stride):
+        patch = torch.from_numpy(input_img[:, i:i+patch_size, j:j+patch_size])
+        patch = patch.unsqueeze(0) # Adds a batch dimension
+        print(patch.shape)
+        with torch.no_grad():
+            prediction = model(patch)
+        result[i:i+patch_size, j:j+patch_size] = prediction
+
+# Export result
+params = src.meta
+params.update(count = 1, dtype='float32')
+with rasterio.open(output_path, 'w', **params) as dest:
+    dest.write_band(1, result)
--- cnn/train.py
+++ cnn/train.py
@@ -3,26 +3,32 @@ from torch.utils.data import DataLoader
 import torch.optim as optim
 import torch.nn as nn
 import torch
-from model import TemperatureCNN
+import logging
+from model import UNet, CNN
 from dataset import RasterPatchDataset
 
+def save_checkpoint(state, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    logging.info('Save checkpoint to {0:}'.format(filename))
+
 # Configuration
-num_epochs = 100
-output_dir = 'patches'
+num_epochs = 30
+output_dir = 'data/patches'
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'Using device: {device}')
 
 # Create DataLoader
 train_dataset = RasterPatchDataset(os.path.join(output_dir, 'train'))
-#test_dataset = RasterPatchDataset(os.path.join(output_dir, 'test'))
+test_dataset = RasterPatchDataset(os.path.join(output_dir, 'test'))
 
 train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
-#test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
 
 # Initialize model, loss, and optimizer
-model = TemperatureCNN().to(device)
+model = CNN().to(device)
+#model = UNet(in_channels=3, out_channels=1).to(device)
 criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=1e-5)
 
 # Training loop
 for epoch in range(num_epochs):
@@ -30,8 +36,12 @@ for epoch in range(num_epochs):
     for inputs, targets in train_loader:  # Assuming dataloader provides batches of data
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
+        #print(inputs.shape)
         outputs = model(inputs)
+        targets = targets.unsqueeze(1) # Add channel dimension
+        #print(outputs.shape)
         loss = criterion(outputs, targets)
         loss.backward()
         optimizer.step()
+        save_checkpoint(model.state_dict())
     print(f'Epoch {epoch+1}, Loss: {loss.item()}')
