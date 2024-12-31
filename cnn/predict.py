e6c01b1da9676922ef5d4e7d8fb5126de20449ee
Andres <andreees.ys@gmail.com>
2024-12-31 12:52:23 +0100

    Save each patch independently

--- cnn/predict.py
+++ cnn/predict.py
@@ -1,5 +1,6 @@
 from smooth_tiled_predictions import predict_img_with_smooth_windowing
 import rasterio
+from rasterio.transform import from_origin
 import torch
 import numpy as np
 from model import CNN
@@ -11,6 +12,10 @@ model_path = 'checkpoint.pth.tar'
 with rasterio.open(input_path) as src:
     # Read all bands from input and target rasters
     input_img = src.read() # Shape: (3, H, W)
+
+# Get size and origin
+pixel_size_x, pixel_size_y = src.transform.a, -src.transform.e
+origin_x, origin_y = src.transform.c, src.transform.f
 
 # Smooth blending parameters
 window_size = 160
@@ -43,10 +48,29 @@ for i in range(0, height - patch_size + 1, stride):
         print(patch.shape)
         with torch.no_grad():
             prediction = model(patch)
-        result[i:i+patch_size, j:j+patch_size] = prediction
+
+        # Update metadata
+        params = src.meta.copy()
+        params.update({
+            'width': patch_size,
+            'height': patch_size,
+            'transform': from_origin(
+                origin_x + j * pixel_size_x,
+                origin_y - i * pixel_size_y,
+                pixel_size_x,
+                -pixel_size_y
+            ),
+            'count': 1,  # Output has a single band
+            'dtype': 'float32'
+        })
+
+        output_path = f'./data/results/patch_{i}_{j}.tif'
+        with rasterio.open(output_path, 'w', **params) as dest:
+            dest.write(prediction.squeeze().numpy(), 1)
+        #result[i:i+patch_size, j:j+patch_size] = prediction
 
 # Export result
-params = src.meta
+'''params = src.meta
 params.update(count = 1, dtype='float32')
 with rasterio.open(output_path, 'w', **params) as dest:
-    dest.write_band(1, result)
+    dest.write_band(1, result)'''
