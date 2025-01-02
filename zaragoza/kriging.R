17e8e6565edde88db24d31f0e83c2c0cfd95c1c8
Andres <andreees.ys@gmail.com>
2025-01-02 17:13:05 +0200

    Correct variogram

--- zaragoza/kriging.R
+++ zaragoza/kriging.R
@@ -16,6 +16,16 @@ data <- read_csv("~/University/uhi/zaragoza/data.csv")
 # Check the first few rows of the dataset
 head(data)
 
+# Check correlation
+cor(data$temp_diff, data$svf, use = "complete.obs")
+cor(data$temp_diff, data$imd, use = "complete.obs")
+cor(data$temp_diff, data$ndvi, use = "complete.obs")
+
+# Standardize covariates to have mean 0 and standard deviation 1
+#data$imd <- scale(data$imd)
+#data$svf <- scale(data$svf)
+#data$ndvi <- scale(data$ndvi)
+
 points <- data.frame(
   lon = data$lon,
   lat = data$lat,
@@ -36,13 +46,19 @@ proj4string(points) <- CRS("+proj=longlat +datum=WGS84")
 str(points)
 
 # Define the variogram model
-variogram_model <- vgm(psill = 1, model = "Sph", range = 1000, nugget = 0.1)
+#variogram_model <- vgm(psill = 1, model = "Sph", range = 1000, nugget = 0.1)
+fitted_variogram <- fit.variogram(
+  variogram(temp_diff ~ svf + imd + ndvi, data = points),
+  model = vgm("Sph")
+)
+print(fitted_variogram)
+plot(variogram(temp_diff ~ svf + imd + ndvi, data = points), fitted_variogram)
 
 # Perform cross-validation to evaluate the model's predictive performance
 cv_results <- krige.cv(
   formula = temp_diff ~ svf + imd + ndvi, # Specify the response variable and covariates
   locations = points, # Spatial data points
-  model = variogram_model, # Variogram model
+  model = fitted_variogram, # Variogram model
   nfold = 10 # Number of folds for cross-validation
 )
 print(cv_results)
@@ -60,10 +76,10 @@ cat("RMSE:", rmse, "\n")
 
 # --- INTERPOLATION ---
 # Paths to the .tif files
-svf_path <- "~/University/uhi/data/rasters/Zaragoza_ETRS89_Sky_View_Factor.tif"
-imd_path <- "~/University/uhi/data/rasters/Zaragoza_ETRS89_Imperviousness_Density_normalized.tif"
-ndvi_path <- "~/University/uhi/data/rasters/Zaragoza_ETRS89_NDVI.tif"
-swir2_path <- "~/University/uhi/data/rasters/Zaragoza_ETRS89_SWIR2_normalized.tif"
+svf_path <- "~/University/uhi/data/rasters/Zaragoza_ETRS89_Sky_View_Factor_scaled.tif"
+imd_path <- "~/University/uhi/data/rasters/Zaragoza_ETRS89_Imperviousness_Density_normalized_scaled.tif"
+ndvi_path <- "~/University/uhi/data/rasters/Zaragoza_ETRS89_NDVI_scaled.tif"
+swir2_path <- "~/University/uhi/data/rasters/Zaragoza_ETRS89_SWIR2_normalized_scaled.tif"
 
 # Load the .tif files as raster layers
 svf_raster <- raster(svf_path)
@@ -89,14 +105,14 @@ covariates_spdf <- as(covariates_stack, "SpatialPixelsDataFrame")
 proj4string(points) <- proj4string(template)
 
 # Define the variogram model
-variogram_model <- vgm(psill = 1, model = "Sph", range = 1000, nugget = 0.1)
+#variogram_model <- vgm(psill = 1, model = "Sph", range = 1000, nugget = 0.1)
 
 # Perform kriging interpolation
 kriging_result <- krige(
   formula = temp_diff ~ svf + imd + ndvi,  # Interpolation formula
   locations = points,                     # Spatial data points
   newdata = covariates_spdf,              # Raster stack as spatial grid
-  model = variogram_model                 # Variogram model
+  model = fitted_variogram                 # Variogram model
 )
 
 # Convert the kriging result back to a raster
